516b0caf7b7e46333b534008eef2cc6d6c648582
lucas da silva cruz <lucas6jau@gmail.com>
2024-04-15 18:33:49 -0300

    test: test for angle

--- src/test/java/mica/MainTest.java
+++ src/test/java/mica/MainTest.java
@@ -1,5 +1,6 @@
 package mica;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
@@ -7,14 +8,27 @@ import static org.assertj.core.api.Assertions.*;
 
 public class MainTest {
 
+    private Robo robotito;
+    @BeforeEach
+    void SetUp(){
+        robotito = new Robo();
+    }
+
+
     @Test
     @DisplayName("Should Accept valid values of coordinates")
-    public void ValidValuesForXandY(){
-    Robo robotito = new Robo();
+    public void ValidValuesXandY(){
+
     robotito.setX_coordenate(100);
     robotito.setY_coordenate(90);
     assertThat(robotito.Coordenates()).isEqualTo("(100,90)");
 
 
+    }
+    @Test
+    @DisplayName("should accept valid values for beam")
+    public void ValidValuesForBeam(){
+        robotito.setAngle(45);
+        assertThat(robotito.getAngle()).isEqualTo(45);
     }
 }
